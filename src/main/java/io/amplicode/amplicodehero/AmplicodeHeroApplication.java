16a9ddbdb468f9568528d12cd165473e9f972cba
Georgii Vlasov <honest.niceman@gmail.com>
2023-11-08 18:02:12 +0400

    Init empty project

---
+++ src/main/java/io/amplicode/amplicodehero/AmplicodeHeroApplication.java
@@ -0,0 +1,13 @@
+package io.amplicode.amplicodehero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmplicodeHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmplicodeHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/amplicode/amplicodehero/AmplicodeHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.amplicode.amplicodehero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplicodeHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

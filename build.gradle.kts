a32ae672e4ed5127d2090fd09f5a325c00fad14f
Georgii Vlasov <honest.niceman@gmail.com>
2023-11-08 18:36:32 +0400

    Add Spring Cloud Server Config

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-config-server")
 }
 
 tasks.withType<Test> {
--- src/main/java/io/amplicode/amplicodehero/AmplicodeHeroApplication.java
+++ src/main/java/io/amplicode/amplicodehero/AmplicodeHeroApplication.java
@@ -2,8 +2,10 @@ package io.amplicode.amplicodehero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AmplicodeHeroApplication {
 
 	public static void main(String[] args) {
